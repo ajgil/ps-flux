68f608a582c808a0eb223b5f62776e08a540d775
Antonio Jesús Gil Palomino <ajgp04@jclm.es>
2020-09-30 14:11:33 +0200

    initial

---
+++ src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2>About me</h2>
+        <p>Esta app usa reac</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default AboutPage;
---
+++ src/components/CourseList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import propTypes from "prop-types";
+
+function CourseList(props) {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author ID</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.courses.map((course) => {
+          return (
+            <tr key={course.id}>
+              <td>{course.title}</td>
+              <td>{course.author}</td>
+              <td>{course.category}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+CourseList.propTypes = {
+  courses: propTypes.array.isRequired,
+};
+
+CourseList.defaultProps = {
+  courses: [],
+};
+
+export default CourseList;
